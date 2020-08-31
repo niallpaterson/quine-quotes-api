9ef40d00cff695610d160a0a75658998e1d47c34
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-31 16:25:14 +0100

    test: add no route tests

--- test/endpoints.int.test.js
+++ test/endpoints.int.test.js
@@ -7,3 +7,21 @@ jest.mock('fs');
 beforeEach(() => {
   db.reset();
 })
+
+describe('It returns a 404 error with the correct message for empty', () => {
+  test('get routes', async () => {
+    const noRoute = await request(api).get('/noroute');
+    expect(noRoute.statusCode).toBe(404);
+    expect(noRoute.text).toBe('This route has no content.');
+  }),
+  test('post routes', async () => {
+    const noRoute = await request(api).post('/noroute');
+    expect(noRoute.statusCode).toBe(404);
+    expect(noRoute.text).toBe('This route has no content.');
+  }),
+  test('delete routes', async () => {
+    const noRoute = await request(api).delete('/noroute');
+    expect(noRoute.statusCode).toBe(404);
+    expect(noRoute.text).toBe('This route has no content.');
+  })
+})
