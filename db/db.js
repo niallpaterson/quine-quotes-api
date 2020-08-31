21802f45fee05e20c80c6cb8cea4f9d8383d4555
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-31 16:15:11 +0100

    feat(db): add db destroy method

--- db/db.js
+++ db/db.js
@@ -5,10 +5,13 @@ const db = './db/quotes.json';
 const read = () => JSON.parse(fs.readFileSync(db, 'utf8'));
 const write = (data) => fs.writeFileSync(db, JSON.stringify(data, null, 1), 'utf8');
 const append = (quote) => write([...read(), quote]);
+const destroy = (id) => write(read().filter((quote) => quote.id !== id));
+
 const data = {
   read,
   write,
   append,
+  destroy
 }
 
 export default data;
