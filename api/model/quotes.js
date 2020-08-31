1847a739a1b2a36ab498cc51240be304d69e5889
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-31 16:18:13 +0100

    feat(model): add create method

--- api/model/quotes.js
+++ api/model/quotes.js
@@ -1,10 +1,13 @@
+import uniqid from 'uniqid';
 import data from '../../db/db.js';
 import getRandom from '../../utility/getRandom.js';
 
 const findAll = () => data.read();
 const findRandom = () => getRandom(data.read());
+const create = ({ quote }) => data.append({ quote, id: uniqid(), dateCreated: new Date().toUTCString() });
 
 const quotes = {
+  create,
   findRandom,
   findAll,
 };
