a4a706886e4059507ec4f9697fdfaf9cb957b6e5
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-31 16:14:46 +0100

    feat(db): add db append method

--- db/db.js
+++ db/db.js
@@ -4,9 +4,11 @@ const db = './db/quotes.json';
 
 const read = () => JSON.parse(fs.readFileSync(db, 'utf8'));
 const write = (data) => fs.writeFileSync(db, JSON.stringify(data, null, 1), 'utf8');
+const append = (quote) => write([...read(), quote]);
 const data = {
   read,
   write,
+  append,
 }
 
 export default data;
