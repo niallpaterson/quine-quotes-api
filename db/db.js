f0c03c053de8c0ef2de0ecb6cbcecdb40bb59a24
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-31 16:14:13 +0100

    feat(db): add db write method

--- db/db.js
+++ db/db.js
@@ -3,8 +3,10 @@ import fs from 'fs';
 const db = './db/quotes.json';
 
 const read = () => JSON.parse(fs.readFileSync(db, 'utf8'));
+const write = (data) => fs.writeFileSync(db, JSON.stringify(data, null, 1), 'utf8');
 const data = {
   read,
+  write,
 }
 
 export default data;
