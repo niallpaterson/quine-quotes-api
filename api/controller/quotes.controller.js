0c47ca2858c925bb4a6bb79ac5cfec76a206e161
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-31 16:20:46 +0100

    feat(controller): add findRandom method

--- api/controller/quotes.controller.js
+++ api/controller/quotes.controller.js
@@ -5,6 +5,11 @@ const findAll = (req, res) => {
   res.status(200).type('json').send(quoteList);
 }
 
+const findRandom = (req, res) => {
+  const randomQuote = quotes.findRandom();
+  res.status(200).type('json').send(randomQuote);
+}
+
 
 const controller = {
   findAll,
