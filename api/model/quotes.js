b1687d5a6919fe0b71feabf9a7baef07a08e69c9
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-08-31 16:18:35 +0100

    feat(model): add destroy method

--- api/model/quotes.js
+++ api/model/quotes.js
@@ -5,11 +5,13 @@ import getRandom from '../../utility/getRandom.js';
 const findAll = () => data.read();
 const findRandom = () => getRandom(data.read());
 const create = ({ quote }) => data.append({ quote, id: uniqid(), dateCreated: new Date().toUTCString() });
+const destroy = (id) => data.destroy(id);
 
 const quotes = {
   create,
   findRandom,
   findAll,
+  destroy,
 };
 
 export default quotes;
